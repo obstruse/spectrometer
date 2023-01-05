71e6e03e781e918533755504fbac78bf45018c35
neisius <neisius@earthlink.net>
2023-01-05 13:33:54 -0800

    closer...

--- python/calibrate.py
+++ python/calibrate.py
@@ -17,20 +17,20 @@ os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = "hide"
             mouse button down:
                 get position
                 right side:
-                    new EU611 value
+                    new col611 value
                 left side:
-                    new Hg436 value
+                    new col436 value
                 create current calibration layer (transparent)
 
             key up:
                 K_KP1:
-                    decrement Hg436 value
-                K_KP7:
-                    increment Hg436 value
+                    decrement col436 value
                 K_KP3:
-                    decrement Eu611 value
+                    decrement col436 value
+                K_KP7:
+                    increment col611 value
                 K_KP9:
-                    increment Hg436 value
+                    increment col611 value
 
                 create current calibration layer (transparent)
 
@@ -41,7 +41,7 @@ os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = "hide"
                     save calibration in calibration.csv
 
             if e.type == QUIT or (e.type == KEYDOWN and e.key == K_ESCAPE):
-                going = False
+                active = False
 
             blt graph
             blt calibration
@@ -65,7 +65,6 @@ CSVfile = str(pathlib.Path(args.CSVfile).with_suffix('.csv'))
 CALfile = str(pathlib.Path(args.CSVfile).with_suffix('.cal'))
 JPGfile = str(pathlib.Path(args.CSVfile).with_suffix('.jpg'))
 BASEfile = os.path.basename(args.CSVfile)
-print(f"CSVfile: {CSVfile}, CALfile: {CALfile}, JPGfile: {JPGfile}")
 
 # read data file
 DATAfile = CSVfile
@@ -75,12 +74,19 @@ with open(DATAfile, mode='r') as file:
     data = list(csv.reader(file))
 
 # calibration values from CSV file
-Hg436 = int(data[0][2])
-Eu611 = int(data[0][3])
-print(f"Hg436: {Hg436}, Eu611: {Eu611}")
+col436 = int(data[0][2])
+col611 = int(data[0][3])
+nm436 = 436
+nm611 = 611
 
 # width determined by length of CSV file; height is 256 for data
 resolution = (len(data)-1,256)
+
+# Calibration options
+CFL = { 'type':'CFL', 'nmLeft':436, 'nmRight':611, 'mask':0b0001, 'nmLandmarks':(405, 487, 542,546)}
+CIS = { 'type':'CIS', 'nmLeft':474, 'nmRight':595, 'mask':0b1110, 'nmLandmarks':(536,)}
+CALS = ( CFL, CIS )
+CALindex = 0         # normally CFL options, toggle to use CIS options
 
 (x,y) = (0,0)
 
@@ -97,9 +103,10 @@ BLACK = (0,0,0)
 RED   = (255,0,0)
 BLUE  = (128,128,255)     # lightened
 GREEN = (0,255,0)
-CYAN = (0,255,255)
+CYAN  = (0,255,255)
 
-use_CIS = False         # normally CFL landmarks, toggle to use CIS landmarks
+m = 0   # multiplier for scaling
+b = 0   # offset for scaling
 
 # surfaces
 # display surface
@@ -115,15 +122,27 @@ graphSurface = pygame.surface.Surface(resolution)
 calibrateSurface = pygame.surface.Surface(resolution)
 
 # rectangles
-EUrect = pygame.Rect((resolution[0]/2,0), (resolution[0]/2,resolution[1]))
-HGrect = pygame.Rect((0,0),               (resolution[0]/2,resolution[1]))
+rectRight = pygame.Rect((resolution[0]/2,0), (resolution[0]/2,resolution[1]))
+rectLeft  = pygame.Rect((0,0),               (resolution[0]/2,resolution[1]))
 
 # create graph
 def createGraph():
-    lastPos = (0,0)
+    mask = CALS[CALindex]['mask']
+    
     graphSurface.fill(BLACK)
     graphSurface.set_colorkey(BLACK)
-    if use_CIS:
+
+    if mask & 0b0001:
+        lastPos = (0,0)
+        for P in data[1::]:
+            currentPos = (int(P[0]),resolution[1]-float(P[1]))
+            if lastPos != (0,0) :
+                pygame.draw.line(graphSurface,WHITE,lastPos,currentPos,2)
+
+            lastPos = currentPos
+
+    if mask & 0b0010:
+        lastPos = (0,0)
         for P in range(resolution[0]):
             currentPos = (P, resolution[1] - backgroundSurface.get_at((P,10))[0])
             if lastPos != (0,0) :
@@ -131,6 +150,7 @@ def createGraph():
 
             lastPos = currentPos
 
+    if mask & 0b0100:
         lastPos = (0,0)
         for P in range(resolution[0]):
             currentPos = (P, resolution[1] - backgroundSurface.get_at((P,10))[1])
@@ -139,6 +159,7 @@ def createGraph():
 
             lastPos = currentPos
 
+    if mask & 0b1000:
         lastPos = (0,0)
         for P in range(resolution[0]):
             currentPos = (P, resolution[1] - backgroundSurface.get_at((P,10))[2])
@@ -147,15 +168,8 @@ def createGraph():
 
             lastPos = currentPos
 
-    else:
-        for P in data[1::]:
-            currentPos = (int(P[0]),resolution[1]-float(P[1]))
-            if lastPos != (0,0) :
-                pygame.draw.line(graphSurface,WHITE,lastPos,currentPos,2)
 
-            lastPos = currentPos
-
-# draw dashed line
+# draw vertical dashed line
 def dashedVLine(surface, xPos, len, color=WHITE, dashLen=8, width=1) :
     yLast = 0
     yPos = 0
@@ -168,65 +182,41 @@ def dashedVLine(surface, xPos, len, color=WHITE, dashLen=8, width=1) :
         yPos += dashLen
 
 
-# create calibrate
+# convert nm to column number
+def nmCol(nm):
+    return nm*m-b
+
+# create calibrate surface
 def calibrate():
-    m = (Eu611 - Hg436) / (611.0 - 436.0) 
-    b = 611.0 * m - Eu611
+    global m, b
+    m = (col611 - col436) / (nm611 - nm436) 
+    b = nm611 * m - col611
 
     calibrateSurface.fill(BLACK)
     calibrateSurface.set_colorkey(BLACK)
 
-    # calibration points
-    #pygame.draw.line(calibrateSurface,RED,(Hg436,0),(Hg436,resolution[1]),2)
-    #pygame.draw.line(calibrateSurface,RED,(Eu611,0),(Eu611,resolution[1]),2)
+    nmLeft      = CALS[CALindex]['nmLeft']
+    nmRight     = CALS[CALindex]['nmRight']
+    nmLandmarks = CALS[CALindex]['nmLandmarks']
 
-    if use_CIS :
-        # calibration points
-        dashedVLine(calibrateSurface,474*m-b,resolution[1],WHITE,12,3)
-        dashedVLine(calibrateSurface,595*m-b,resolution[1],WHITE,12,3)
+    # calibration points
+    dashedVLine(calibrateSurface,nmCol(nmLeft) ,resolution[1],WHITE,12,3)
+    dashedVLine(calibrateSurface,nmCol(nmRight),resolution[1],WHITE,12,3)
         
-        # CIS landmarks
-        # https://photo.stackexchange.com/questions/122037/why-do-typical-imaging-sensor-colour-filter-spectral-responses-differ-so-much-fr
-        #landmarks = [475,510,580]
-        landmarks = [474,536,595]
-        for L in landmarks:
-            P = L*m-b
-            #pygame.draw.line(calibrateSurface,CYAN,(P,0),(P,resolution[1]),3)
-            dashedVLine(calibrateSurface,P,resolution[1],WHITE,4,1)
-    else:
-        # calibration points
-        dashedVLine(calibrateSurface,Hg436,resolution[1],WHITE,12,3)
-        dashedVLine(calibrateSurface,Eu611,resolution[1],WHITE,12,3)
-        # cfl landmarks
-        landmarks = [405, 436, 487, 542, 546, 611]
-        for L in landmarks:
-            P = L*m-b
-            #pygame.draw.line(calibrateSurface,BLUE,(P,0),(P,resolution[1]),1)
-            dashedVLine(calibrateSurface,P,resolution[1],WHITE,4,1)
+    # landmarks
+    for L in nmLandmarks:
+        dashedVLine(calibrateSurface,nmCol(L),resolution[1],WHITE,4,1)
 
     ## LED landmarks
     ## https://gpnmag.com/article/white-leds-for-plant-applications/
     #landmarks = [432,556,637]
-    #for L in landmarks:
-    #    P = L*m-b
-    #    pygame.draw.line(calibrateSurface,GREEN,(P,0),(P,resolution[1]),1)
-
-    ## CIS landmarks
-    ## https://photo.stackexchange.com/questions/122037/why-do-typical-imaging-sensor-colour-filter-spectral-responses-differ-so-much-fr
-    #landmarks = [475,510,580]
-    #for L in landmarks:
-    #    P = L*m-b
-    #    pygame.draw.line(calibrateSurface,CYAN,(P,0),(P,resolution[1]),3)
-
 
     # camera limits
-    limits = [400,700]
-    for L in limits:
-        P = L*m-b
+    nmLimits = [400,700]
+    for L in nmLimits:
+        P = nmCol(L)
         pygame.draw.line(calibrateSurface,RED,(P,7*resolution[1]/8),(P,resolution[1]),3)
 
-def nmCol(nm):
-    return nm*m-b
 
 createGraph()
 calibrate()
@@ -240,8 +230,8 @@ pygame.display.flip()
 # throttle
 timer = pygame.time.Clock()
 
-going = True
-while going:
+active = True
+while active:
     timer.tick(10)
 
     events = pygame.event.get()
@@ -249,45 +239,44 @@ while going:
         if (e.type == MOUSEBUTTONDOWN):
             # get mouse position
             (x,y) = pygame.mouse.get_pos()
-            if HGrect.collidepoint((x,y)):
-                if use_CIS:
-                    m = (Eu611 - x) / (611.0 - 475.0) 
-                    b = 611.0 * m - Eu611
-                    Hg436 = nmCol(436)
-                else:
-                    Hg436 = x
+            if rectLeft.collidepoint((x,y)):
+                nmLeft = CALS[CALindex]['nmLeft']
+                m = (col611 - x) / (nm611 - nmLeft) 
+                #b = nm611 * m - col611
+                b = nmLeft * m - x
+                col436 = nmCol(nm436)
             
-            if EUrect.collidepoint((x,y)):
-                if use_CIS:   
-                    m = (x - Hg436) / (580.0 - 436.0) 
-                    b = 580.0 * m - x
-                    Eu611 = nmCol(611)
-                else:
-                    Eu611 = x
-
+            if rectRight.collidepoint((x,y)):
+                nmRight = CALS[CALindex]['nmRight'] 
+                m = (x - col436) / (nmRight - nm436) 
+                #b = nm436 * m - col436
+                b = nmRight * m - x
+                col611 = nmCol(nm611)
+            
             calibrate()
 
         if e.type == KEYUP :
             if e.key == K_KP1:
-                Hg436 -= 1
+                col436 -= 1
             if e.key == K_KP3:
-                Hg436 += 1
+                col436 += 1
 
             if e.key == K_KP7:
-                Eu611 -= 1
+                col611 -= 1
             if e.key == K_KP9:
-                Eu611 += 1
+                col611 += 1
 
             if e.key == K_c:
-                use_CIS = not use_CIS
+                CALindex += 1
+                CALindex = CALindex % len(CALS)
                 createGraph()
 
             calibrate()
 
             if e.key == K_s:        # save calibration in .CAL file
                 with open(CALfile,'w') as CAL:
-                    data[0][2] = Hg436
-                    data[0][3] = Eu611
+                    data[0][2] = col436
+                    data[0][3] = col611
                     C = csv.writer(CAL)
                     C.writerows(data)
 
@@ -295,7 +284,7 @@ while going:
             # 
 
         if e.type == QUIT or (e.type == KEYDOWN and e.key == K_ESCAPE):
-            going = False
+            active = False
 
         lcd.blit(backgroundSurface,(0,0))
         lcd.blit(graphSurface,(0,0))
